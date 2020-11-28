2e52aab8595b62e2a384b8eb55cb4cb44be611f8
Manu Rajput <manurajput2911@gmail.com>
2020-11-29 02:26:14 +0530

    Adding a vector to store characters with their frequencies, still has some bug

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <queue>
+#include<vector>
 using namespace std;
-class node{
+class _singlechar{
     public:
     char data;
     int freq;
-    node* left;
-    node* right;
-    node(char data,int freq){
+    _singlechar(char data){
+        this->data=data;
+        this->freq=0;
+    }
+    void increment(){
+        this->freq++;
+    }
+};
+class _node{
+    public:
+    char data;
+    int freq;
+    _node* left;
+    _node* right;
+    _node(char data,int freq){
         this->data=data;
         this->freq=freq;
         this->left=NULL;
@@ -16,12 +29,12 @@ class node{
 };
 
 struct compare{
-    bool operator()(node*l,node*r){
+    bool operator()(_node*l,_node*r){
         return(l->freq > r->freq);
     }
 };
 
-void printtree(node* root,string code){
+void printtree(_node* root,string code){
     //cout<<code<<" t4"<<endl;
     if(root==NULL){
         return;
@@ -37,10 +50,10 @@ void printtree(node* root,string code){
 
 void huffmantree(char data[],int freq[],int size){
     //cout<<"t1"<<endl;
-    node* left,*right,*top;
-    priority_queue<node*, vector<node*>, compare> q ;
+    _node* left,*right,*top;
+    priority_queue<_node*, vector<_node*>, compare> q ;
     for(int i=0;i<size;i++){
-        q.push(new node(data[i],freq[i]));
+        q.push(new _node(data[i],freq[i]));
     }
     /*
     for(int i=0;i<size;i++){
@@ -54,7 +67,7 @@ void huffmantree(char data[],int freq[],int size){
         q.pop();
         right=q.top();
         q.pop();
-        top=new node('#',left->freq+right->freq);
+        top=new _node('#',left->freq+right->freq);
         top->left=left;
         top->right=right;
         q.push(top);
@@ -63,13 +76,47 @@ void huffmantree(char data[],int freq[],int size){
     printtree(q.top(),"");
 
 }
-
-int main(){
+void ins(vector<_singlechar*> &a,char b){
+    auto i=(a.begin()+1);
+    cout<<(*i)->data;
+    while(i!=a.end()){
+        if((*i)->data>b){
+            break;
+        }
+        i++;
+    }
+    if((*(i-1))->data==b){
+        (*(i-1))->freq++;
+    }
+    else{
+        a.insert(i,new _singlechar(b));
+    }
+    /*
+    for(auto i=a.begin();i!=a.end();i++){
+       cout<<(*i)->data<<" : "<<(*i)->freq<<endl;
+    }
+    */
    
+
+}
+int main(){
+   vector<_singlechar*> a;
+   //vector<int> b;
+   //b.insert(b.begin(),1);
+   string str;
+   cin>>str;
+   int size = str.size();
+   a.push_back(new _singlechar(str[0]));
+   for(int i=1;i<size;i++){
+       ins(a,str[i]);
+   }
+   for(auto i=a.begin();i!=a.end();i++){
+       cout<<(*i)->data<<" : "<<(*i)->freq<<endl;
+   }
+   /*
    char arr[]={'a','b','c','d','e','f'};
    int freq[]={5,9,12,13,16,45};
-   int size = sizeof(arr)/sizeof(arr[0]);
    huffmantree(arr,freq,size); 
-
+*/
    return 0;
 }
