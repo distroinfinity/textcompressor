3bd7a0906ca8e7bfec28efa3aa38bf5e5022836a
Manu Rajput <manurajput2911@gmail.com>
2020-12-04 22:34:11 +0530

    reading and writing compressed data and encoding back right

--- main.cpp
+++ main.cpp
@@ -5,6 +5,30 @@
 #include<fstream>
 using namespace std;
 
+int binary_to_decimal(string in)
+{
+	int result = 0;
+	for (int i = 0; i < in.size(); i++)
+		result = result * 2 + in[i] - '0';
+	return result;
+}
+
+string decimal_to_binary(int in)
+{
+	string temp = "";
+	string result = "";
+	while (in)
+	{
+		temp += ('0' + in % 2);
+		in /= 2;
+	}
+	result.append(8 - temp.size(), '0');													//append '0' ahead to let the result become fixed length of 8
+	for (int i = temp.size() - 1; i >= 0; i--)												
+	{
+		result += temp[i];
+	}
+	return result;
+}
 class _code{
     public:
     char character;
@@ -110,8 +134,8 @@ void compress(){
     //string str
     //cin>>str;
     //int size = str.size();
+    
     char c;
-
     ifstream read;
     read.open("input_string.txt");
     if(read.fail()){
@@ -127,11 +151,11 @@ void compress(){
     //vector to store characters along with their coding
     vector<_code*> coding;
     huffmantree(coding,a,a.size()); 
-    /*
+    
     for(auto i=coding.begin();i!=coding.end();i++){
-        cout<<(*i)->character<<" "<<(*i)->code<<endl;
+        cout<<(*i)->character<<"writing"<<(*i)->code<<endl;
         }
-    */
+    
     //auto i=coding.begin();
     
     /*
@@ -143,8 +167,11 @@ void compress(){
     */
     //int j=0;
     //ifstream read;
+    /*
     vector<bool> encoded_data;
+    cout<<encoded_data.max_size()<<endl;
     read.open("input_string.txt");
+    int count1=0,count2=0;
     while(read.get(c)){
         auto i=coding.begin();
         while(i!=coding.end()){
@@ -164,12 +191,63 @@ void compress(){
                 break;
             }
             i++;
+            count1++;
         }
     }
     cout<<encoded_data.size()<<endl;
     read.close();
-    ofstream write;
-    write.open("output.bin",ios::binary|ios::out);
+    */
+    ofstream out_file;
+    ifstream in_file;
+    in_file.open("input_string.txt",ios::in);
+    out_file.open("output.bin",ios::binary|ios::out);
+    string in ="",s="";
+     //the first byte saves the size of the vector
+    //in+=(char)coding.size();
+    in+=coding.size();
+    for(auto i=coding.begin();i!=coding.end();i++){
+        in+=(int)(*i)->character;
+        //set the codes with a fixed 128-bit string form[000¡­¡­1 + real code]
+        //'1' indicates the start of huffman code
+        s.assign(127-((*i)->code).size(),'0');
+        s+='1';
+        s.append((*i)->code);
+        in+=(char)binary_to_decimal(s.substr(0,8));
+        for (int i = 0; i < 15; i++)
+		{//cut into 8-bit binary codes that can convert into saving char needed for binary file
+			s = s.substr(8);
+			in += (char)binary_to_decimal(s.substr(0, 8));
+		}
+    }
+    s.clear();
+    char b;
+    in_file.get(b);
+    while(!in_file.eof()){
+        //convert char by char
+        auto i=coding.begin();
+        while((*i)->character!=b){
+            i++;
+        }
+        s+=((*i)->code);
+        while(s.size()>8){
+            //chop into 8 bits
+            in+=(char)binary_to_decimal(s.substr(0,8));
+            s=s.substr(8);
+        }
+        in_file.get(b);
+    }
+    int count = 8-s.size();
+    if(s.size()<8){
+        s.append(count,'0');
+    }
+    in+=(char)binary_to_decimal(s);
+    in+=(char)count;
+    cout<<in<<endl;
+    cout<<in.size()<<endl;
+    out_file.write(in.c_str(),in.size());
+    in_file.close();
+    out_file.close();
+    /*
     int Bit_Counter = 0;
 	uint8_t Packed_Byte = 0;
     
@@ -184,10 +262,12 @@ void compress(){
 		if(Bit_Counter == 8){
 			write << Packed_Byte;
 			Bit_Counter = 0;
+            Packed_Byte = 0;
 		}
 	}
     write << Packed_Byte;
     write.close();
+    */
 }
 char search(vector<_code*> &coding,string binary){
     auto i=coding.begin();
@@ -199,11 +279,73 @@ char search(vector<_code*> &coding,string binary){
     }
     return (*i)->character;
 }
+
 void decode(){
     //class _code{ char character; string code }
+    ifstream in_file;
+    in_file.open("output.bin",ios::in|ios::binary);
+    unsigned char size;
+    in_file.read(reinterpret_cast<char*>(&size),1);
+
     //vector to store characters along with their decoding
     vector<_code*> decode;
-    char charac;
+    for(int i=0;i<size;i++){
+        char a_code;
+        unsigned char h_code_c[16];
+        in_file.read(&a_code,1);
+        
+        in_file.read(reinterpret_cast<char*>(h_code_c),16);
+        string h_code_s="";
+        for(int i=0;i<16;i++){
+            //obtain original 128 bit-binary string
+            h_code_s+=decimal_to_binary(h_code_c[i]);
+        }
+        int j=0;
+        while(h_code_s[j]=='0'){
+            //delete the added '0000iii1' to get the real huffman code
+            j++;
+        }
+        h_code_s=h_code_s.substr(j+1);
+        cout<<a_code<<"reading"<<h_code_s<<endl;
+        decode.push_back(new _code(a_code,h_code_s));
+    }
+    in_file.close();
+    in_file.open("output.bin",ios::in|ios::binary);
+    fstream out_file;
+    out_file.open("decoded_text.txt",ios::out);
+    //unsigned char size;
+    //get number of codings
+    in_file.read(reinterpret_cast<char*>(&size),1);
+    //jump to the last one byte to get the number of '0' append to the string at last
+    in_file.seekg(-1,ios::end);
+    char count0;
+    in_file.read(&count0,1);
+    //jump to the position where text starts
+    in_file.seekg(1+17*size,ios::beg);
+    vector<unsigned char> text;
+    unsigned char textseg;
+    in_file.read(reinterpret_cast<char*>(&textseg),1);
+    while(!in_file.eof()){
+        //get the text byte by byte using unsigned char
+        text.push_back(textseg);
+        in_file.read(reinterpret_cast<char*>(&textseg),1);
+    }
+    string path;
+    string complete="";
+    for(int i=0;i<text.size()-1;i++){
+        //translate the huffman code
+        path=decimal_to_binary(text[i]);
+        if(i==text.size()-2){
+            path = path.substr(0,8-count0);
+        }
+        complete=complete+path;
+    }
+    cout<<complete<<endl;
+    in_file.close();
+    out_file.close();
+
+    /*
+    char c;
     string str="",line;
     int size;
 
@@ -234,6 +376,7 @@ void decode(){
     }
     read.close();
     write.close();
+    */
     /*
     for(auto i=decode.begin();i!=decode.end();i++){
         cout<<(*i)->character<<" "<<(*i)->code<<endl;
@@ -251,7 +394,7 @@ int main(){
             compress();
             break;
             case 2:
-            cout<<"test1"<<endl;
+            //cout<<"test1"<<endl;
             decode();
             break;
             case -1:
--- testiobin.cpp
+++ testiobin.cpp
@@ -5,34 +5,52 @@
 #include <bitset>
 #include<stdio.h>
 using namespace std;
-int main() 
-{ 
-  std::string bit_string = "110010"; 
-  std::bitset<8> b1(bit_string);             // [0, 0, 1, 1, 0, 0, 1, 0] 
-  //bitset<8> * a=&b1;
-  // string from position 2 till end 
-  std::bitset<8> b2(bit_string, 2);      // [0, 0, 0, 0, 0, 0, 1, 0] 
-  
-  // string from position 2 till next 3 positions 
-  std::bitset<8> b3(bit_string, 2, 3);   // [0, 0, 0, 0, 0, 0, 0, 1] 
-  //std::cout << b1 << '\n' << b2 << '\n' << b3 << '\n'; 
-  
-  ofstream write;
-  write.open("testbin.bin",ios::binary | ios::out);
-  //write.write(false,sizeof(bool));
-  uint8_t t=00000000;
-  char a ='i';
-  string b= "0001";
-  write<<a<<b;
-  write<<t;
-  //cout<<t;
-  //int *a=&e;
-  //int *b=&e;
-  //FILE *p;
-  //p=fopen("testbin.bin","wb");
-  //fwrite(a,sizeof(bitset<8>),sizeof(a),p);
-  //fwrite(b,1,sizeof(a),p); 
-  write.close();
 
+int binary_to_decimal(string& in)
+{
+	int result = 0;
+	for (int i = 0; i < in.size(); i++)
+		result = result * 2 + in[i] - '0';
+	return result;
+}
+
+string decimal_to_binary(int in)
+{
+	string temp = "";
+	string result = "";
+	while (in)
+	{
+		temp += ('0' + in % 2);
+		in /= 2;
+	}
+	result.append(8 - temp.size(), '0');													//append '0' ahead to let the result become fixed length of 8
+	for (int i = temp.size() - 1; i >= 0; i--)												
+	{
+		result += temp[i];
+	}
+	return result;
+}
+
+int main() 
+{ /*
+  ofstream write;
+  write.open("testbin.bin",ios::binary|ios::out);
+  char a='i';
+  write.write(&a,sizeof(char));
+  write.close();
+  */
+string a="manu";
+a+=1;
+cout<<a;
+ /*
+ ifstream read;
+  read.open("output.bin",ios::binary | ios::in);
+   char textseg;
+	read.read(reinterpret_cast(&textseg), 1);
+  string temp = decimal_to_binary(textseg);
+  cout<<temp<<endl;
+
+  read.close();
+ */ 
   return 0; 
 }  
